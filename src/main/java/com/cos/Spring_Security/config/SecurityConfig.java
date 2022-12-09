f3b01f53e728764150a0e66e38389d52d2957d43
kkm5291 <kkm23125291@gmail.com>
2022-12-09 23:00:26 +0900

    feat : 구글 로그인 추가

--- src/main/java/com/cos/Spring_Security/config/SecurityConfig.java
+++ src/main/java/com/cos/Spring_Security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.Spring_Security.config;
 
+import com.cos.Spring_Security.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -12,6 +14,9 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록이 됨
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // securedEnable : secured 어노테이션 활성화, prePostEnabled : preAuthorization을 활성화
 public class SecurityConfig {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
 
     // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해준다.
@@ -32,7 +37,12 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해줌. 이 말인 즉슨 controller에 /login을 만들지 않아도 됨
-                .defaultSuccessUrl("/"); // 특정페이지로 로그인하면 특정페이지로 넘어가게 알아서 해줌..... 미쳤다
+                .defaultSuccessUrl("/") // 특정페이지로 로그인하면 특정페이지로 넘어가게 알아서 해줌..... 미쳤다
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리가 필요함.
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
 
         return http.build();
     }
